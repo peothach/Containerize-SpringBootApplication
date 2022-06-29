289566c14f80d11ce22e73058667572c4721c48a
Thach. Nguyen Huu - CMC Global DU3.11 <peothach@gmail.com>
2022-06-29 12:35:10 +0700

    update resources

---
+++ src/main/java/com/containerize/ContainerizeApplication.java
@@ -0,0 +1,21 @@
+package com.containerize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ContainerizeApplication {
+
+	@GetMapping("/")
+	public String home(){
+		return "Hello Docker World";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContainerizeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/containerize/ContainerizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.containerize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContainerizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
